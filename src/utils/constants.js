1337a799a671f88dd425a9f8569b0efa723fa4cb
Ayush Chauhan <ayushchauhan212@gmail.com>
2024-05-07 09:14:11 +0530

    fixed cors issue

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,6 +2,6 @@ export const LOGO_URL = "https://public.bnbstatic.com/image/pgc/202312/b29531739
 
 export const IMG_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/";
 
-export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
+export const MENU_API = "https://zwigato-server-eolq.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&&submitAction=ENTER&restaurantId=";
 
-export const RES_CARD_API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.4484257&lng=78.5684594&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+export const RES_CARD_API = "https://zwigato-server-eolq.onrender.com/api/restaurants?lat=25.4484257&lng=78.5684594&page_type=DESKTOP_WEB_LISTING";
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { RES_CARD_API } from './src/utils/constants'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {RES_CARD_API}
-
-    }
-  },
   plugins: [react()],
-})
+});
